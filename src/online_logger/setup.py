f368782b771ad697eee6ec3d4732ac53c091beb7
MagicTINTIN <49071368+MagicTINTIN@users.noreply.github.com>
2025-11-20 22:33:09 +0000

    feat: adding logger subscriber

---
+++ src/online_logger/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'online_logger'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='gaspar',
+    maintainer_email='gaspar@todo.todo',
+    description='Logger that sends logs to server',
+    license='GPL-3.0-only',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'online_logger = online_logger.LoggerSubscriber:main'
+        ],
+    },
+)
